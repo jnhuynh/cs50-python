0acc81b6c5675349a644557ffe12455f24a14730
John Huynh <john@huynh.io>
2023-09-12 17:08:14 -0500

    1.2

---
+++ 1.conditionals/bank.py
@@ -0,0 +1,26 @@
+def greeting_payment(greeting=''):
+    # Cleanup input
+    greeting = greeting.lower().strip()
+
+    # Find occurence of hello
+    # if greeting.startswith('hello'):
+    if greeting.startswith('hello'):
+        return 0
+
+    # Find occurence of leading `h`
+    if greeting.startswith('h'):
+        return 20
+
+    # Improper greeting, pay full amount
+    return 100
+
+
+def main():
+    # Prompt for greeting
+    greeting = input('What is your greeting? ')
+
+    amount = greeting_payment(greeting)
+    print(f'${amount}')
+
+
+main()
